aff17b598082386bd6be1fb0956c450337b499bc
Omar Flores Grimontt <omar.flores@rappi.com>
2022-03-12 11:11:51 -0500

    removed things

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      GET OUT MF
+    <div
+      style={{
+        fontSize: 120,
+        display: 'flex',
+        height: 'calc(100vh - 120px)',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <span>🐛</span>
     </div>
-  )
+  );
 }
